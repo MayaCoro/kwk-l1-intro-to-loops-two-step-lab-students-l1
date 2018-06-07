69ce74107fbf321f4a684231d183f1d6f768a0a6
MayaCoro <mayascoro@gmail.com>
2018-06-07 18:49:11 +0000

    automatically backed up by learn

---
+++ loops.rb
@@ -0,0 +1,33 @@
+#10.times do
+  #puts "WHERE'S MY LUNCH?"
+#end
+#loop do
+  #puts "rumble grumble"
+  #puts "I'm so hungry"
+  #puts ":("
+#end
+
+#def dance 
+#   moves_slay = 0 
+#   loop do
+#     if moves_slay == 10
+#       break
+#     end
+#   puts "slide to the left"
+#   puts "slide to the right"
+#   puts "kick"
+#   moves_slay=moves_slay+1
+# end
+# end
+# dance
+
+def kwk_camp(days)
+  days = 10
+  until days == 0 do
+  puts "Go to camp, Maya."
+  puts "You have #{days} days left."
+  days = days - 1
+end
+puts "Awww. Camp is over."
+end
+kwk_camp(10)
